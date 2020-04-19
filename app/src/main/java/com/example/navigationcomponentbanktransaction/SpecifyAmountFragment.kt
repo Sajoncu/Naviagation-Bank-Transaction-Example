dbad64ec07fc5261c34e4e2aec6e0c45083dec01
Saiful Islam Sajon <saifulislamsajon@gmail.com>
2020-04-19 14:10:43 +0600

    all the fragment layouts has been styled

--- app/src/main/java/com/example/navigationcomponentbanktransaction/SpecifyAmountFragment.kt
+++ app/src/main/java/com/example/navigationcomponentbanktransaction/SpecifyAmountFragment.kt
@@ -9,7 +9,7 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass.
  */
-class SpecificAmountFragment : Fragment() {
+class SpecifyAmountFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
