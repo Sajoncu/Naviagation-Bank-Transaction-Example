67a90407d8a9cd4bc85c83295d308a7910d851cf
Saiful Islam Sajon <saifulislamsajon@gmail.com>
2020-04-19 15:57:40 +0600

    ChooseRecipient fragments functionality has been completed

--- app/src/main/java/com/example/navigationcomponentbanktransaction/ChooseRecipientFragment.kt
+++ app/src/main/java/com/example/navigationcomponentbanktransaction/ChooseRecipientFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
-class ChooseRecipientFragment : Fragment() {
-    
+class ChooseRecipientFragment : Fragment(), View.OnClickListener {
+    var navController: NavController? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,4 +22,17 @@ class ChooseRecipientFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_choose_recipient, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.cancel_btn)
+        view.findViewById<Button>(R.id.next_btn)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.next_btn -> navController!!.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
+            R.id.send_money_btn -> requireActivity().onBackPressed()
+        }
+    }
 }
--- app/src/main/java/com/example/navigationcomponentbanktransaction/SpecifyAmountFragment.kt
+++ app/src/main/java/com/example/navigationcomponentbanktransaction/SpecifyAmountFragment.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
 
 /**
  * A simple [Fragment] subclass.
  */
 class SpecifyAmountFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
